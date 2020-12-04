ccaf4dfc1829fa15508c5754d1a1e28bd00cbd89
Tiko <65791040+TikoA@users.noreply.github.com>
2020-12-04 15:07:14 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 
 function App() {
-
+  const [lang, setLang] = useState('EN')
+  
   return (
     <div className="App">
-      <Welcome />
-      <About />     
-      <Experience />
-      <Skills /> 
+      <Welcome lang={lang} setLang={setLang}/>
+      <About lang={lang}/>     
+      <Experience lang={lang}/>
+      <Skills lang={lang}/> 
     </div>
   );
 }
